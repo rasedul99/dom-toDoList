a2c8ad0110dfbaa4bf3387f9077d0c535735e6f7
rasedul99 <rasedul2199@gmail.com>
2022-02-11 17:50:55 +0600

    delete function added

---
+++ project.js
@@ -0,0 +1,25 @@
+let count = 0;
+document.getElementById("add-item-btn").addEventListener("click", function () {
+  const inputValue = document.getElementById("input-value").value;
+  if (inputValue == "") {
+    alert("please enter a value");
+  } else {
+    count++;
+    const newTableRow = document.createElement("tr");
+
+    newTableRow.innerHTML = ` 
+  <th>${count}</th>
+  <td>${inputValue}</td>
+  <td><button class="btn btn-success delete-btn">Delete</button> <button class='btn btn-danger done-btn'>Done</button></td>`;
+    document.getElementById("table-body").appendChild(newTableRow);
+    document.getElementById("input-value").value = "";
+
+    const deleteBtn = document.getElementsByClassName("delete-btn");
+    // console.log(deletesBtn);
+    for (const button of deleteBtn) {
+      button.addEventListener("click", function (e) {
+        e.target.parentNode.parentNode.style.display = "none";
+      });
+    }
+  }
+});
